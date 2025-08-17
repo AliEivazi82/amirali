4442737a831aa157b30dfe530acf699ee8f2eed8
Ali Eivazi <codealiea@gmail.com>
2025-08-17 19:16:21 +0330

    commit NO.2

---
+++ 24.py
@@ -0,0 +1,5 @@
+user = int(input("number: "))
+
+for i in range(1,11):
+    # print(user * i)
+    print(f"{user} x {i} = {user * i}")
---
+++ 25.py
@@ -0,0 +1,5 @@
+def salam(Fname, Lname):
+    print(f"salam {Lname} halet chetore?")
+
+
+salam("Amirali", "Kahrizi")
