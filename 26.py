8b3cb5962063e9bd4dea5150ebac644a3715fd66
Ali Eivazi <codealiea@gmail.com>
2025-08-17 20:19:09 +0330

    26/05

--- 26.py
+++ 26.py
@@ -1,13 +1,16 @@
 import random
 
 def pizza(money:float):
+
     type_of = {
         20:"peperoni",
         35:"meat & mashroom",
         30:"greek",
         25:"garlic"
     }
+
     prices = list(type_of.keys())
+
     for price in prices:
         if price > money:
             print("it's bigger", price)
@@ -15,6 +18,5 @@ def pizza(money:float):
             print("it's smaller", price)
         else:
             print("mosavi", price)
-    
 
 pizza(22)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-while True: # تا همیشه کد های داخل حلقه اجرا میشن !
-    javab_user = input("chi mikhai?") # از کاربر سوال می پرسه
-    if javab_user == "END": # ENDاگر جواب کاربر برابر بود با 
-        print("salam Amirali")
-        pass
-    print("salam")
+dict1 = {
+    456:"salam",
+    "test":"bye"
+}
+
+print(dict1.keys())
