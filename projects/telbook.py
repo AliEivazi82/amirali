df303768b330b603c9b3d7b59bfa84b19e540efe
Ali Eivazi <codealiea@gmail.com>
2025-09-18 20:07:49 +0330

    27/6/1404

---
+++ projects/telbook.py
@@ -0,0 +1,14 @@
+
+while True:
+    print("1)Add\n2)Read\n3)End")
+    user = input("Entekhab kon: ")
+
+    if user == "end":
+        print("ok, Googbye🙌")
+        break
+    elif user == "add":
+        pass # TODO
+    elif user == "read":
+        pass # TODO
+    else:
+        print("lotfan yki az gozine ha ro entekhab kon!!!")
